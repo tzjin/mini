96497507ca4bf5b4f19cd626f82d8307c606d56f
Tony Jin <tzjin@princeton.edu>
2014-02-27 14:54:26 -0500

    Trie can now return all values from a prefix

--- trie/Trie.java
+++ trie/Trie.java
@@ -1,8 +1,8 @@
 /* Author: Tony Jin
  * Date: 2/26/14
  * 
- * Description: Implement a Trie data structure that stores
- *              and returns strings.
+ * Description: Implement a Trie (prefix tree) data structure that 
+ *              stores and returns strings.
  *
  * Dependencies: java.util.*
  *
@@ -10,7 +10,6 @@
  * Runtime:     % java Trie
  *
  * Next steps:  Reduce read times by preclaculating values (doubly linked)
- *              Method to return all possibilities (array vs linkedlist)
  *              Sort output lexicographically
  *              Sort output by most recent acess date
  *
@@ -20,26 +19,19 @@ import java.util.*;
 
 public class Trie {
 
+    // Node in prefix tree
     private class Node {
-        private Character a;
         private HashMap<Character, Node> children;
         private boolean end;
 
-        public Node (char c) {
+        public Node() {
             children = new HashMap<Character, Node>();
-            a = c;
+            end = false;
         }
 
         public Node add(char c) {
-            Node next;
-            if (hasChild(c))
-                next = children.get(c);
-            else {
-                next = new Node(a);
-                children.put(c, next);
-            }
-
-            return next;      
+            if (!hasChild(c)) children.put(c, new Node());
+            return next(c);      
         }
 
         public Node next(char c) {
@@ -53,16 +45,19 @@ public class Trie {
         private boolean hasChild(char c) {
             return children.containsKey(c);
         }
-
     }
 
     private Node head;
+
+    public Trie() {
+        head = new Node();
+    }
 
     public Trie(String word) { 
         if (word.length() == 0)
             throw new IllegalArgumentException();
 
-        head = new Node(word.charAt(0));
+        head = new Node();
         add(word);
     }
 
@@ -77,20 +72,50 @@ public class Trie {
     public boolean contains(String word) {
         Node current = head;
         for (int i = 0; i < word.length(); i++) {
+            current = current.next(word.charAt(i));
             if (current == null)
                 return false;
-            current = current.next(word.charAt(i));
         }
 
         return current.end;
     }
 
-    public String []
+    public LinkedList<String> getPoss(String prefix) {
+        Node current = head;
+        for (int i = 0; i < prefix.length(); i++) {
+            current = current.next(prefix.charAt(i));
+            if (current == null)
+                return null;
+        }
+
+        LinkedList<String> list = new LinkedList<String>();
+
+        helper(current, prefix, list);
+
+        return list;
+    }
+
+    private void helper(Node node, String prefix, LinkedList<String> list) { 
+        if (node.end) list.addLast(prefix);
+
+        for(Character c : node.children.keySet()) 
+            helper(node.children.get(c), prefix + c, list);
+        
+    }
 
     public static void main(String [] args) {
-        Trie trie = new Trie("hit");
+        Trie trie = new Trie("bo");
         trie.add("hi");
+        trie.add("him");
+        trie.add("hiss");
         System.out.println(trie.contains("hi"));
         System.out.println(trie.contains("boo"));
+        System.out.println(trie.contains("hit")); 
+        System.out.println(trie.contains("h"));
+        System.out.println(trie.contains(""));
+        LinkedList<String> ls = trie.getPoss("h");
+        System.out.println();
+        for(String s : ls)
+            System.out.println(s);
     }    
 }
