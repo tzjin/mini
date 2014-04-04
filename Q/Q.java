7d58a9ee61bdc305fcbc445311146268f266abf1
Tony Jin <tzjin@princeton.edu>
2014-04-04 00:43:17 -0400

    Added Q

---
+++ Q/Q.java
@@ -0,0 +1,70 @@
+/* Q.java
+ *
+ * Tony Jin
+ * 4/4/13
+ *
+ * Description:  the concise Queue
+ *
+ * Dependencies: java.util.LinkedList
+ */
+
+import java.util.LinkedList;
+
+public class Q<Item> {
+
+    private LinkedList<Item> q;
+
+    /* Constructors */
+
+    // initializing an empty queue
+    public Q() {
+        q = new LinkedList<Item>();
+    }
+
+    // initalizing a queue populated with array elements
+    public Q(Item [] items) {
+        q = new LinkedList<Item>();
+        for (int i = 0; i < items.length; i++)
+            nq(items[i]);
+    }
+
+    // initializing a queue based on an iterator's elements
+    public Q(Iterable<Item> items) {
+        q = new LinkedList<Item>();
+        for (Item i : items) 
+            nq(i);
+    }
+
+
+    /* Access Methods */
+
+    // isEmpty
+    public boolean em() {
+        return (q.size() == 0);
+    }
+
+    // enqueue
+    public void nq(Item item) {
+        q.add(item);
+    }
+
+    // dequeue
+    public Item dq() {
+        return q.remove();
+    }
+
+    // see (peek)
+    public Item c() {
+        return q.peek();
+    }
+
+
+    /* Tests */
+    public static void main (String [] args) {
+        Q<String> q = new Q<String>(args);
+
+        while (!q.em())
+            System.out.print(q.dq() + " ");
+        System.out.println();
+    }
+}
---
+++ trie/Dialer.java
@@ -0,0 +1,9 @@
+// Object that utilises a trie to return possible numbers
+
+public class Dialer {
+    private Trie trie = new Trie();
+
+    public static void main() {
+        
+    }
+}
